0743eedc820ff15bad7141797c53d23282256885
Maurits Lourens <lourens_M@hotmail.com>
2016-08-30 14:11:13 +0200

    initial commit - chapter 02

---
+++ app/app.component.ts
@@ -0,0 +1,17 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <StackLayout>
+      <Image src="res://logo_login" stretch="none" horizontalAlignment="center"></Image>
+      <TextField hint="Email Address" keyboardType="email" autocorrect="false" autoCapitalizationType="none"></TextField>
+      <TextField hint="Password" secure="true"></TextField>
+
+      <Button class="submit-button" text="Sign in"></Button>
+      <Button text="Sign up for Groceries"></Button>
+    </StackLayout>
+  `,
+  styleUrls: ['pages/login/login-common.css', 'pages/login/login.css']
+})
+export class AppComponent {}
